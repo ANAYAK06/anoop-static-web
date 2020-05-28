be300664cc6ea9ecc1b7f4d106ba0a387ee37cf3
Anoop <anoop2u@outlook.com>
2020-05-28 12:50:23 +0530

    personal web site project finished

---
+++ scripts/hireapp.js
@@ -0,0 +1,11 @@
+document.getElementById('h').addEventListener('click',
+function(){
+    document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('.pop-close').addEventListener('click', 
+function(){
+    document.querySelector('.bg-model').style.display ='none';
+
+});
+
---
+++ scripts/server.js
@@ -0,0 +1,11 @@
+document.getElementById('s').addEventListener('click',
+function(){
+    document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('.pop-close').addEventListener('click', 
+function(){
+    document.querySelector('.bg-model').style.display ='none';
+
+});
+
