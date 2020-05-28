f989fa1f75b79427a5d78daba67e1df0a338dbbb
Anoop <anoop2u@outlook.com>
2020-05-28 11:09:07 +0530

    finsihed pop-up job for one not made mandatory fileds in jv script

---
+++ scripts/contacts.js
@@ -0,0 +1,11 @@
+document.getElementById('hire-button').addEventListener('click',
+function(){
+    document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('.pop-close').addEventListener('click', 
+function(){
+    document.querySelector('.bg-model').style.display ='none';
+
+});
+
